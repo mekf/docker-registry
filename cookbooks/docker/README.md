a801dee1117fce40ab2139faa2655b246f96a569
Minh Ha <minh@enspiral.com>
2014-12-05 14:04:15 +1300

    init docker cookbook

---
+++ cookbooks/docker/README.md
@@ -0,0 +1,3 @@
+docker Cookbook
+===============
+Practising writing cookbook, install docker on CentOS 6.5
---
+++ cookbooks/docker/metadata.rb
@@ -0,0 +1,7 @@
+name             'docker'
+maintainer       'Minh Ha'
+maintainer_email 'minh@enspiral.com'
+license          'MIT'
+description      'Installs/Configures docker'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
